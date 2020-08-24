87a4ad689165cd9aa6cae98d3a1fba478f85ffc3
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2020-08-25 00:50:05 +0100

    Updates

---
+++ precache-manifest.2f8684e7be507a4249d583ab6154a793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efceae5fb8d51adec0a455978cd7d283",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "134f24037d495fea63fc",
+    "url": "/Expense-Tracker/static/css/main.8af48bb8.chunk.css"
+  },
+  {
+    "revision": "cb413a8c1444e6271bcb",
+    "url": "/Expense-Tracker/static/js/2.ec66af10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Expense-Tracker/static/js/2.ec66af10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134f24037d495fea63fc",
+    "url": "/Expense-Tracker/static/js/main.eb231e5e.chunk.js"
+  },
+  {
+    "revision": "75f2794814e1e6b57aab",
+    "url": "/Expense-Tracker/static/js/runtime-main.a6d46df4.js"
+  }
+]);
